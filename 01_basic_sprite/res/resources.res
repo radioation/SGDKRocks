5f8126e9b6997d7df4ee04f6789246fe333501e1
Greg Gallardo <greggallardo@gmail.com>
2021-02-14 19:39:02 -0600

    adding a second sprite from Pro Motion NG

--- 01_basic_sprite/res/resources.res
+++ 01_basic_sprite/res/resources.res
@@ -1,1 +1,2 @@
 SPRITE ship "sprites/ship.png" 2 2 NONE
+SPRITE rock "sprites/rock.png" 2 2 NONE
--- 01_basic_sprite/src/main.c
+++ 01_basic_sprite/src/main.c
@@ -4,22 +4,27 @@
 int main(u16 hard)
 {
 	// set backgorund color.  
-	VDP_setBackgroundColor(1);
+	VDP_setBackgroundColor(0);
 
 	// Sprite Pointer
 	Sprite *shipSprite = NULL;
+	Sprite *rockSprite = NULL;
 
 	// load the palette of the ship resource
 	VDP_setPalette( PAL1, ship.palette->data );
+	VDP_setPalette( PAL2, rock.palette->data );
 
 	// Initialize the Sprite engine
 	SPR_init(0,0,0);
 
 	// create the sprite from the resource
 	int ship_pos_x = 147;
-	int ship_pos_y = 110;
+	int ship_pos_y = 150;
 	shipSprite = SPR_addSprite( &ship, ship_pos_x, ship_pos_y, TILE_ATTR( PAL1, 0, FALSE, FALSE ));	
 		
+	int rock_pos_x = 147;
+	int rock_pos_y =  60;
+	rockSprite = SPR_addSprite( &rock, rock_pos_x, rock_pos_y, TILE_ATTR( PAL2, 0, FALSE, FALSE ));	
 
 	while(TRUE)
 	{
@@ -30,6 +35,7 @@ int main(u16 hard)
 		}*/
 
 		SPR_setPosition( shipSprite, ship_pos_x, ship_pos_y );
+		SPR_setPosition( rockSprite, rock_pos_x, rock_pos_y );
 
 		// draw current screen (logo, start screen, settings, game, gameover, credits...)
 		SPR_update();
