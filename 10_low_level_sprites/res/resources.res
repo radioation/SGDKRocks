54ff68e573fc9d2e2328a17e2ebcdde6bd084c90
Greg Gallardo <greggallardo@gmail.com>
2023-02-09 01:25:23 -0600

    Adding more sprites low level sprite example

---
+++ 10_low_level_sprites/res/resources.res
@@ -0,0 +1,5 @@
+TILESET ship_tileset "sprites/ship.png" NONE NONE
+TILESET rock_tileset "sprites/rock.png" NONE NONE
+
+PALETTE ship_pal "sprites/ship.png" 
+PALETTE rock_pal "sprites/rock.png" 
--- 10_low_level_sprites/src/main.c
+++ 10_low_level_sprites/src/main.c
@@ -1,6 +1,7 @@
 #include <genesis.h>
+#include "resources.h"
 
-const u32 tile[8] =
+const u32 square_tile[8] =
 {
   0x11111111,
   0x12222221,
@@ -24,13 +25,27 @@ int main(bool hard)
 
   int ind = TILE_USER_INDEX;
 
-  VDP_loadTileData( tile, // tile data pointer
+  VDP_loadTileData( square_tile, // tile data pointer
       ind,    // index
       1,        // number of tiles to load 
       DMA_QUEUE         // transfer method
       );
 
-  VDP_setSprite(0, // sprite ID ( 0 to 79 )
+  int ship_ind = ind + 1;
+  VDP_loadTileData( ship_tileset.tiles, // tile data pointer
+      ship_ind,    // index
+      4,        // number of tiles to load 
+      DMA_QUEUE         // transfer method
+      );
+
+  int rock_ind = ship_ind + 4; // 4 because we loaded 4 tiles for ship
+  VDP_loadTileData( rock_tileset.tiles, // tile data pointer
+      rock_ind,    // index
+      4,        // number of tiles to load 
+      DMA_QUEUE         // transfer method
+      );
+
+  VDP_setSpriteFull(0, // sprite ID ( 0 to 79 )
       40,   // X in screen coords
       40,   // Y in screen coords
       SPRITE_SIZE(1,1), // 1x1 to up to 4x4
@@ -39,11 +54,57 @@ int main(bool hard)
         0,  // Flip Vertical
         0,  // Flip Horizontal
         ind // index
-        ) 
+        ),
+      1 // link to next sprite
       );
+
+  VDP_setSpriteFull(1, // sprite ID ( 0 to 79 )
+      60,   // X in screen coords
+      40,   // Y in screen coords
+      SPRITE_SIZE(1,1), // 1x1 to up to 4x4
+      TILE_ATTR_FULL(PAL0,    // PALette 
+        1,  // priority
+        0,  // Flip Vertical
+        0,  // Flip Horizontal
+        ind // index
+        ) ,
+        2 // link to next sprite
+      );
+
+
+  VDP_setSpriteFull(2, // sprite ID ( 0 to 79 )
+      80,   // X in screen coords
+      40,   // Y in screen coords
+      SPRITE_SIZE(2,2), // 1x1 to up to 4x4
+      TILE_ATTR_FULL(PAL1,    // PALette 
+        1,  // priority
+        0,  // Flip Vertical
+        0,  // Flip Horizontal
+        ship_ind // index
+        ) ,
+      3
+      );
+
+
+  VDP_setSpriteFull(3, // sprite ID ( 0 to 79 )
+      100,   // X in screen coords
+      40,   // Y in screen coords
+      SPRITE_SIZE(2,2), // 1x1 to up to 4x4
+      TILE_ATTR_FULL(PAL2,    // PALette 
+        1,  // priority
+        0,  // Flip Vertical
+        0,  // Flip Horizontal
+        rock_ind // index
+        ) ,
+      4
+      );
+
+
   // tell VDP to draw sprites
-  VDP_updateSprites(1, // number of sprites to transfer starting at index 0
-      DMA_QUEUE);
+  VDP_updateSprites(4, DMA_QUEUE_COPY);
+ 
+  //  VDP_updateSprites(80, // number of sprites to transfer starting at index 0
+   //   DMA_QUEUE);
 
 
   while(TRUE)
