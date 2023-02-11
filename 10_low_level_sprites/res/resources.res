b7082b30f9f6823d0c8f188e7b71ed99fe96f1ff
Greg Gallardo <greggallardo@gmail.com>
2023-02-11 01:34:53 -0600

    Adding animation frames and movement to low level sprite example

--- 10_low_level_sprites/res/resources.res
+++ 10_low_level_sprites/res/resources.res
@@ -1,7 +1,9 @@
 TILESET ship_tileset "sprites/ship.png" NONE NONE
 TILESET rock_tileset "sprites/rock.png" NONE NONE
 TILESET numbers_tileset "sprites/numbers.png" NONE NONE
+TILESET shipsheet_tileset "sprites/shipsheet.png" NONE NONE
 
 PALETTE ship_pal "sprites/ship.png" 
 PALETTE rock_pal "sprites/rock.png" 
 PALETTE numbers_pal "sprites/numbers.png" 
+PALETTE shipsheet_pal "sprites/shipsheet.png" 
--- 10_low_level_sprites/src/main.c
+++ 10_low_level_sprites/src/main.c
@@ -20,10 +20,13 @@ int main(bool hard)
   VDP_setBackgroundColor(0);
 
   // load the palettes
-  PAL_setPalette( PAL1, ship_pal.data, CPU );
-  PAL_setPalette( PAL2, rock_pal.data, CPU );
-  PAL_setPalette( PAL3, numbers_pal.data, CPU );
+  PAL_setPalette( PAL0, ship_pal.data, CPU );
+  PAL_setPalette( PAL1, rock_pal.data, CPU );
+  PAL_setPalette( PAL2, numbers_pal.data, CPU );
+  PAL_setPalette( PAL3, shipsheet_pal.data, CPU );
 
+
+  // Load the tilesets
   int ind = TILE_USER_INDEX;
 
   VDP_loadTileData( square_tile, // tile data pointer
@@ -53,7 +56,15 @@ int main(bool hard)
       DMA_QUEUE         // transfer method
       );
 
+  int shipsheet_ind = numbers_ind + 32; 
+  VDP_loadTileData( shipsheet_tileset.tiles, // tile data pointer
+      shipsheet_ind,    // index
+      80,        // number of tiles to load 
+      DMA_QUEUE         // transfer method
+      );
 
+
+  // Add Sprites
   VDP_setSpriteFull(0, // sprite ID ( 0 to 79 )
       40,   // X in screen coords
       40,   // Y in screen coords
@@ -77,7 +88,7 @@ int main(bool hard)
         0,  // Flip Horizontal
         ind // index
         ) ,
-        2 // link to next sprite
+      2 // link to next sprite
       );
 
 
@@ -85,7 +96,7 @@ int main(bool hard)
       80,   // X in screen coords
       40,   // Y in screen coords
       SPRITE_SIZE(2,2), // 1x1 to up to 4x4
-      TILE_ATTR_FULL(PAL1,    // PALette 
+      TILE_ATTR_FULL(PAL0,    // PALette 
         1,  // priority
         0,  // Flip Vertical
         0,  // Flip Horizontal
@@ -99,7 +110,7 @@ int main(bool hard)
       100,   // X in screen coords
       40,   // Y in screen coords
       SPRITE_SIZE(2,2), // 1x1 to up to 4x4
-      TILE_ATTR_FULL(PAL2,    // PALette 
+      TILE_ATTR_FULL(PAL1,    // PALette 
         1,  // priority
         0,  // Flip Vertical
         0,  // Flip Horizontal
@@ -113,24 +124,71 @@ int main(bool hard)
       120,   // X in screen coords
       40,   // Y in screen coords
       SPRITE_SIZE(2,2), // 1x1 to up to 4x4
-      TILE_ATTR_FULL(PAL3,    // PALette 
+      TILE_ATTR_FULL(PAL2,    // PALette 
         1,  // priority
         0,  // Flip Vertical
         0,  // Flip Horizontal
         numbers_ind + 28 // index
         ) ,
-      0
+      5
       );
 
   // tell VDP to draw sprites
-  VDP_updateSprites(5, DMA_QUEUE_COPY);
- 
-  //  VDP_updateSprites(80, // number of sprites to transfer starting at index 0
-   //   DMA_QUEUE);
-
+  s16 x = 160;
+  s16 y = 100;
+  s16 shipframe_offset = 0;
+  s16 shipframe_dir = 1;
+  VDP_setSpriteFull(5, // sprite ID ( 0 to 79 )
+      x,   // X in screen coords
+      y,   // Y in screen coords
+      SPRITE_SIZE(4,4), // 1x1 to up to 4x4
+      TILE_ATTR_FULL(PAL3,    // PALette 
+        1,  // priority
+        0,  // Flip Vertical
+        0,  // Flip Horizontal
+        shipsheet_ind + shipframe_offset  // index
+        ) ,
+      0
+      );
+  // tell VDP to draw  the 6 sprites we've defined.
+  VDP_updateSprites(6, DMA_QUEUE_COPY);
 
   while(TRUE)
   {
+    // move sprite position
+    ++x; 
+
+    // chagne current animiation frame by computing offset in tile memory
+    if ( x % 10 == 0) {
+      if( shipframe_dir > 0 ) {
+        shipframe_offset += 16;
+      } else {
+        shipframe_offset -= 16;
+      }
+      if( shipframe_offset > 64 ) {
+        shipframe_dir = -1;
+        shipframe_offset = 48;
+      }
+      if( shipframe_offset < 0  ) {
+        shipframe_dir = 1;
+        shipframe_offset = 16;
+      }
+    }
+
+    // update position of srpite 5
+    VDP_setSpritePosition(5, // sprite ID ( 0 to 79 )
+        x,   // X in screen coords
+        y   // Y in screen coords
+        );
+
+    // update animation frame of sprite 5 by setting tile index.
+    VDP_setSpriteTile(5, // sprite ID ( 0 to 79 )
+        shipsheet_ind + shipframe_offset  // tile index
+        );
+
+      // tell VDP to draw  the 6 sprites we've defined.
+      VDP_updateSprites(6, DMA_QUEUE_COPY);
+
     // do VBLankd processes
     SYS_doVBlankProcess();
   }
