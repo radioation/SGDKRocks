a9387d7f78397ba4580791eaffa22523ac4c94e4
Greg Gallardo <greggallardo@gmail.com>
2023-02-18 22:15:34 -0600

    minor cleanup

--- 11_low_level_background/src/main.c
+++ 11_low_level_background/src/main.c
@@ -2,46 +2,45 @@
 
 const u32 tile[8]=
 {
-	0xEEEEEEEE,
-	0x44444444,
-	0xEEEEEEEE,
-	0x44444444,
-	0xEEEEEEEE,
-	0x44444444,
-	0xEEEEEEEE,
-	0x44444444
+  0xEEEEEEEE,
+  0x44444444,
+  0xEEEEEEEE,
+  0x44444444,
+  0xEEEEEEEE,
+  0x44444444,
+  0xEEEEEEEE,
+  0x44444444
 };
 
 
 int main(bool arg) 
 {
 
-	// Load the tile
-	int ind = TILE_USER_INDEX;
+  // Load the tile into VRAM with VDP_loadTileData
+  s16 ind = TILE_USER_INDEX; 
+  VDP_loadTileData( (const u32 *)tile, // tile data pointer
+      ind,   // index 
+      1,   // number of tiles to load
+      DMA_QUEUE    // transfer method
+      ); 
 
-	VDP_loadTileData( (const u32 *)tile, // tile data pointer
-			ind,   // index 
-			1,   // number of tiles to load
-			DMA_QUEUE    // transfer method
-			); 
-
-	//write our tile 1 on plane A at (5,5) with pal 0
-	VDP_setTileMapXY(BG_B,
-			TILE_ATTR_FULL( PAL1,// Palette
-				1,                // Priority
-				0,                // Flip Vertical
-				0,                // FLip Horizontal
-				ind),             // tile index
-			5,  // X
-			5); // Y
-
-
-	while(1)
-	{
-		// ... code
-		SYS_doVBlankProcess();
-	}
+  // Copy our tile from VRAM to BG_B at (5,5) with pal 1
+  VDP_setTileMapXY(BG_B,
+      TILE_ATTR_FULL( PAL1,// Palette
+        1,                // Priority
+        0,                // Flip Vertical
+        0,                // FLip Horizontal
+        ind),             // tile index
+      5,  // X
+      5); // Y
 
 
-	return 0;
+  while(1)
+  {
+    // ... code
+    SYS_doVBlankProcess();
+  }
+
+
+  return 0;
 }
