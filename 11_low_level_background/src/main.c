59f75d31871934078f61e9eeb27020c1733814be
Greg Gallardo <greggallardo@gmail.com>
2023-02-19 16:46:14 -0600

    adding 'ground' tiles

--- 11_low_level_background/src/main.c
+++ 11_low_level_background/src/main.c
@@ -12,10 +12,179 @@ const u32 tile[8]=
   0x44444444
 };
 
+const u32 tileSet0[288] =
+{
+  // tile 0 
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x77777777,
+  0x66666666,
+  0x55555555,
+  0x44444444,
+  0x44444444,
+  // tile 1 
+  0x33333333,
+  0x33333333,
+  0x22222222,
+  0x22222222,
+  0x11111111,
+  0x11111111,
+  0x88888888,
+  0x88888888,
+  // tile 2 
+  0x88888888,
+  0x77777777,
+  0x77777777,
+  0x77777777,
+  0x77777777,
+  0x66666666,
+  0x66666666,
+  0x66666666,
+  // tile 3 
+  0x66666666,
+  0x55555555,
+  0x55555555,
+  0x55555555,
+  0x55555555,
+  0x55555555,
+  0x55555555,
+  0x44444444,
+  // tile 4 
+  0x44444444,
+  0x44444444,
+  0x44444444,
+  0x44444444,
+  0x44444444,
+  0x44444444,
+  0x33333333,
+  0x33333333,
+  // tile 5 
+  0x33333333,
+  0x33333333,
+  0x33333333,
+  0x33333333,
+  0x33333333,
+  0x33333333,
+  0x33333333,
+  0x33333333,
+  // tile 6 
+  0x22222222,
+  0x22222222,
+  0x22222222,
+  0x22222222,
+  0x22222222,
+  0x22222222,
+  0x22222222,
+  0x22222222,
+  // tile 7 
+  0x22222222,
+  0x22222222,
+  0x22222222,
+  0x22222222,
+  0x22222222,
+  0x11111111,
+  0x11111111,
+  0x11111111,
+  // tile 8 
+  0x11111111,
+  0x11111111,
+  0x11111111,
+  0x11111111,
+  0x11111111,
+  0x11111111,
+  0x11111111,
+  0x11111111
+};
+
+const u32 tileSet1[72] =
+{
+ // tile 0 
+0x00000000,
+0x00000000,
+0x00000000,
+0x77770000,
+0x66667777,
+0x55556666,
+0x44445555,
+0x44444444,
+ // tile 1 
+0x33334444,
+0x33333333,
+0x22223333,
+0x22222222,
+0x11112222,
+0x11111111,
+0x88881111,
+0x88888888,
+ // tile 2 
+0x88888888,
+0x77778888,
+0x77777777,
+0x77777777,
+0x77777777,
+0x66667777,
+0x66666666,
+0x66666666,
+ // tile 3 
+0x66666666,
+0x55556666,
+0x55555555,
+0x55555555,
+0x55555555,
+0x55555555,
+0x55555555,
+0x44445555,
+ // tile 4 
+0x44444444,
+0x44444444,
+0x44444444,
+0x44444444,
+0x44444444,
+0x44444444,
+0x33334444,
+0x33333333,
+ // tile 5 
+0x33333333,
+0x33333333,
+0x33333333,
+0x33333333,
+0x33333333,
+0x33333333,
+0x33333333,
+0x33333333,
+ // tile 6 
+0x22223333,
+0x22222222,
+0x22222222,
+0x22222222,
+0x22222222,
+0x22222222,
+0x22222222,
+0x22222222,
+ // tile 7 
+0x22222222,
+0x22222222,
+0x22222222,
+0x22222222,
+0x22222222,
+0x11112222,
+0x11111111,
+0x11111111,
+ // tile 8 
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000
+};
 
 int main(bool arg) 
 {
-
+  //////////////////////////////////////////////////////////////////////
   // Load the tile into VRAM with VDP_loadTileData
   s16 ind = TILE_USER_INDEX; 
   VDP_loadTileData( (const u32 *)tile, // tile data pointer
@@ -34,12 +203,14 @@ int main(bool arg)
       5,  // X
       0); // Y
 
+
+  //////////////////////////////////////////////////////////////////////
   // make a row of 40 tiles and load int VRAM
   s16 rowIndex = ind + 1;
-  u32 myTiles[320];
-  memset( myTiles,0, sizeof(myTiles) );
-  memset( myTiles, 0x0E, 32 ); // set the first tile to 0x0E
-  VDP_loadTileData( (const u32 *)myTiles, // tile data pointer
+  u32 rowTiles[320];
+  memset( rowTiles,0, sizeof(rowTiles) );
+  memset( rowTiles, 0x0E, 32 ); // set the first tile to 0x0E
+  VDP_loadTileData( (const u32 *)rowTiles, // tile data pointer
       rowIndex,   // index 
       40,   // number of tiles to load
       DMA_QUEUE    // transfer method
@@ -60,19 +231,44 @@ int main(bool arg)
         ); 
   }
 
+
+  //////////////////////////////////////////////////////////////////////
+  // Load the tileSet0 into VRAM with VDP_loadTileData
+  s16 tileSet0Index = rowIndex + 40;
+  VDP_loadTileData( (const u32 *)tileSet0, // tile data pointer
+      tileSet0Index,   // index 
+      9,   // number of tiles to load
+      DMA_QUEUE    // transfer method
+      ); 
+
+  for( u16 x=1; x< 40; ++x ) {
+    // make a column out of it.
+    VDP_fillTileMapRectInc( BG_B,
+        TILE_ATTR_FULL( PAL2,// Palette
+          1,                // Priority
+          0,                // Flip Vertical
+          0,                // FLip Horizontal
+          tileSet0Index),             // tile index
+        x,  // x
+        20,  // y
+        1,  // width
+        9  // height
+        ); 
+  }
+
   s16 offset=0;  
 
   while(1)
   {
     // update the buffer
-    memset( myTiles+offset, 0x0,32);
+    memset( rowTiles+offset, 0x0,32);
     offset+=8;
     if( offset > 312 ) {
       offset=0;
     }
-    memset( myTiles+offset, 0x0E, 32 );
+    memset( rowTiles+offset, 0x0E, 32 );
     // load into VRAM
-    VDP_loadTileData( (const u32 *)myTiles, // tile data pointer
+    VDP_loadTileData( (const u32 *)rowTiles, // tile data pointer
         rowIndex,   // index 
         40,   // number of tiles to load
         DMA_QUEUE    // transfer method
@@ -84,3 +280,4 @@ int main(bool arg)
 
   return 0;
 }
+
