8186883b425867c0ab6353b08e654ebe8e045e9b
Greg Gallardo <greggallardo@gmail.com>
2024-01-01 12:15:03 -0600

    starting c# tests

---
+++ 12_networking/csharp/simple/client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpEchoClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: TcpEchoClient <server_ip> <message>");
+                return;
+            }
+
+            var serverIp = args[0];
+            var message = args[1];
+
+            try
+            {
+                using (var client = new TcpClient(serverIp, 8080))
+                using (var stream = client.GetStream())
+                {
+                    byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+                    await stream.WriteAsync(messageBytes, 0, messageBytes.Length);
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    var response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                    Console.WriteLine("Received response: {0}", response);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error connecting to server: {0}", e.Message);
+            }
+        }
+    }
+}
---
+++ 12_networking/csharp/simple/server/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TcpEchoServer
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("Usage: TcpEchoServer <server_ip>");
+				return;
+			}
+			var serverIp = args[0];
+			var listener = new TcpListener(IPAddress.Parse(serverIp), 8080);
+			listener.Start();
+
+			while (true)
+			{
+				var client = await listener.AcceptTcpClientAsync();
+				Task.Run(() => HandleClient(client));
+			}
+		}
+
+		static async Task HandleClient(TcpClient client)
+		{
+			try
+			{
+				using (var stream = client.GetStream())
+				{
+					byte[] buffer = new byte[1024];
+					int bytesRead;
+
+					while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+					{
+						await stream.WriteAsync(buffer, 0, bytesRead);
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.Error.WriteLine("Error handling client: {0}", e.Message);
+			}
+			finally
+			{
+				client.Close();
+			}
+		}
+	}
+}
